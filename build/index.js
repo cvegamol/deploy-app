d18fa6bfe35fea2162d0c6391338c0fac38e41ec
cvegamol <cvegmol@gmail.com>
2024-10-26 12:49:37 +0200

    Correccion

--- build/index.js
+++ build/index.js
@@ -1,19 +1,21 @@
 "use strict";
-import operacion from './routes/operacion';
+
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const operacion_1 = __importDefault(require("./routes/operacion"));
+
 const app = (0, express_1.default)();
 //middlewares que transforma la req.body en json
 app.use(express_1.default.json());
 //definimos el puerto
 const PORT = 3000;
+app.use(operacion_1.default);
 app.get('/ping', (req, res) => {
     res.send('Hello World');
 });
-app.use( operacion);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
